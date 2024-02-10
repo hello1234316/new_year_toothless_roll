1aebd8b8f569716a2414277eeca2522b3f96b7e6
Hello Phone <85028651+hello1234316@users.noreply.github.com>
2024-02-10 14:01:20 +0800

    Create script.js

---
+++ claim/script.js
@@ -0,0 +1,8 @@
+// toothless dace new year meme
+const url = "https://www.youtube.com/watch?v=lP5lgPx87sU";
+
+let linkClicker = document.getElementById('link_click');
+
+window.onload = window.onfocus = () => {
+  this.document.location.href = url;
+}
